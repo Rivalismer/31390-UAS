7c1d8c1b966e705022c4b178bb59ea744551b234
Ask <ask.e.oren@gmail.com>
2022-06-09 11:06:05 +0200

    Control part3 done

--- Excercises/Part3/Driver_33.m
+++ Excercises/Part3/Driver_33.m
@@ -29,7 +29,7 @@ phi = 0;
 theta = 0;
 psi = 0;
 z = 0;
-x = 2;
+x = 0;
 y = 2;
 
 uxy = [x
@@ -65,7 +65,6 @@ tf_34 = [L*k, 0, -L*k, 0
 tf_xy = [0 -1 0
     1 0 0];
 
-TF_xy = tf_xy*eye(3);
 % Vector of angular speeds
 Omega = [w0
     w0
@@ -81,52 +80,60 @@ tau = 2*w0*[L*k*(Omega(1) - Omega(3))
     b*(Omega(1) - Omega(2) + Omega(3) - Omega(4))];
 
 %% Plotting
-l = ceil(length(out.Theta(:,1))/2);
-lp = ceil(length(out.p(:,1))/2);
-fp = 3000;
+%l = ceil(length(out.Theta(:,1))/2);
+%lp = ceil(length(out.p(:,1))/2);
+fp = 100000;
+Ta = linspace(0,100,length(out.Theta(:,1)));
+Tp = linspace(0,100,length(out.p(:,1)));
 
 close all;
 figure
 subplot(3,1,1)
-plot(out.Theta(l:(l + fp),1))
-xlabel('Time (ms)')
+%plot(out.Theta(l:(l + fp),1))
+plot(Ta,out.Theta(:,1))
+xlabel('Time (s)')
 ylabel('Value')
 title('\phi - Angle')
-xlim([0 fp])
+%xlim([0 fp])
 
 subplot(3,1,2)
-plot(out.Theta(l:(l + fp),2))
-xlabel('Time (ms)')
+%plot(out.Theta(l:(l + fp),2))
+plot(Ta,out.Theta(:,2))
+xlabel('Time (s)')
 ylabel('Value')
 title('\Theta - Angle')
-xlim([0 fp])
+%xlim([0 fp])
 
 subplot(3,1,3)
-plot(out.Theta(l:(l + fp),3))
-xlabel('Time (ms)')
+%plot(out.Theta(l:(l + fp),3))
+plot(Ta,out.Theta(:,3))
+xlabel('Time (s)')
 ylabel('Value')
 title('\psi - Angle')
-xlim([0 fp])
+%xlim([0 fp])
 
 figure
 subplot(3,1,1)
-plot(out.p(lp:(lp+fp),3))
-xlabel('Time (ms)')
+%plot(out.p(lp:(lp+fp),3))
+plot(Tp,out.p(:,3))
+xlabel('Time (s)')
 ylabel('Value')
 title('z - Value')
-xlim([0 fp])
-ylim([-1 2])
+%xlim([0 fp])
+%ylim([-1 2])
 
 subplot(3,1,2)
-plot(out.p(lp:(lp+fp),2))
-xlabel('Time (ms)')
+%plot(out.p(lp:(lp+fp),2))
+plot(Tp,out.p(:,2))
+xlabel('Time (s)')
 ylabel('Value')
 title('y - Value')
-xlim([0 fp])
+%xlim([0 fp])
 
 subplot(3,1,3)
-plot(out.p(lp:(lp+fp),1))
-xlabel('Time (ms)')
+%plot(out.p(lp:(lp+fp),1))
+plot(Tp,out.p(:,1))
+xlabel('Time (s)')
 ylabel('Value')
 title('x - Value')
-xlim([0 fp])
+%xlim([0 fp])
