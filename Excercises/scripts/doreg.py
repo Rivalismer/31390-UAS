db3d48ea3bf85ba008935d51bde8c4970f740783
Ernestas <simutisernestas@gmail.com>
2022-06-15 10:59:05 +0200

    add PWM mapping to simulink

--- Excercises/scripts/doreg.py
+++ Excercises/scripts/doreg.py
@@ -32,23 +32,21 @@ def plot_regression_line(x, y, b):
     plt.plot(x, y_pred, color="g")
 
     # putting labels
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel('Thrust [N]')
+    plt.ylabel('Thrust [PWM]')
 
     # function to show plot
     plt.show()
 
+if __name__ == "__main__":
+    # observations
+    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
 
-# observations
-x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+    # estimating coefficients
+    b = linear_regression(x, y)
+    print("Estimated coefficients:\nb_0 = {}  \
+            \nb_1 = {}".format(b[0], b[1]))
 
-# estimating coefficients
-b = linear_regression(x, y)
-print("Estimated coefficients:\nb_0 = {}  \
-        \nb_1 = {}".format(b[0], b[1]))
-
-# plotting regression line
-plot_regression_line(x, y, b)
-
-
+    # plotting regression line
+    plot_regression_line(x, y, b)
