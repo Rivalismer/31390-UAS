35d05c17d753cef79e674bcbce336442f1a4f0f7
Ask <ask.e.oren@gmail.com>
2022-06-24 19:09:38 +0200

    7.3 almost done

--- Excercises/Part2/Rb.m
+++
@@ -1,9 +0,0 @@
-function pout = Rb(pin, psi, theta, phi)
-
-rbi = [cos(psi)*cos(theta), cos(psi)*sin(theta)*sin(phi) - sin(psi)*cos(phi), sin(psi)*sin(phi) + cos(psi)*sin(theta)*cos(phi)
-    sin(psi)*cos(theta), cos(psi)*cos(phi) + sin(psi)*sin(theta)*sin(phi), sin(psi)*sin(theta)*sin(phi) - cos(psi)*sin(phi)
-    -sin(theta), cos(theta)*sin(phi), cos(theta)*cos(phi)];
-
-pout = rbi*pin;
-end
-
--- Excercises/Part6/pwmplot.m
+++
@@ -1,9 +0,0 @@
-clc
-close all
-
-ts = squeeze(out.thrust.time);
-data = squeeze(out.thrust.data);
-
-plot(ts,data)
-xlabel("t [s]")
-ylabel("Thrust [N]")
